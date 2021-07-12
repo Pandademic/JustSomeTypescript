e4664ee41892e094add53c8162dfcb2b7e414d19
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-07-12 17:33:30 -0400

    Create hello.kt

---
+++ koltin/hello.kt
@@ -0,0 +1,15 @@
+var HelloYes=false
+fun main() {
+    println("Hello, world!!!")
+    hello()
+}
+fun hello(){
+    println("Hello")
+    if (HelloYes==true){
+        println("Hello yes is true")
+    }
+    else{
+        println("Hello world is not true")
+        println(false)
+    }
+}
